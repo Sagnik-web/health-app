2cad5f1be9f37f4714ce9e785c5bbd9b79ba8633
Sagnik Biswas <75833389+Sagnik-web@users.noreply.github.com>
2024-03-15 23:21:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "build/index.html"));
+//   });
+// }
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
